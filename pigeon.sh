0ce6c26119c4f75988fb8e606b736fd8aa7c9bbd
Radoslav Penev <radoslav.penev@miracl.com>
2025-06-13 10:34:36 +0300

    Add Swift Package Manager support

--- pigeon.sh
+++ pigeon.sh
@@ -1,6 +1,6 @@
 dart run pigeon \
   --input pigeon/sdk_types.dart \
   --dart_out lib/src/pigeon.dart \
-  --swift_out ios/Classes/Pigeon.swift \
+  --swift_out ios/flutter_miracl_sdk/Sources/flutter_miracl_sdk/Pigeon.swift \
   --kotlin_out android/src/main/kotlin/com/miracl/trust/flutter_miracl_sdk/Pigeon.kt \
   --kotlin_package "com.miracl.trust.flutter_miracl_sdk"
