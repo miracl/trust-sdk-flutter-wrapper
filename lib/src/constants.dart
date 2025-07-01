73db125f9abb6a66f2be5bafe0d0582eb9e3a113
Neno Stefanov <neno.st.stefanov@gmail.com>
2025-07-01 14:15:40 +0300

    Update version to 0.5.1

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,2 +1,2 @@
-const sdkApplicationInfo = "flutter_miracl_sdk/0.5.0";
+const sdkApplicationInfo = "flutter_miracl_sdk/0.5.1";
 const pluginNotInitializedErrorMessage = "The MIRACL Trust Flutter plugin is not initialized!";
