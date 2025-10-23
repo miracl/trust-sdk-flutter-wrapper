08081e21bff803acb2301b56c114ec42dfccf363
Radoslav Penev <radoslav.penev@miracl.com>
2025-10-23 15:35:48 +0300

    Bump version to 0.7.0

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,2 +1,2 @@
-const sdkApplicationInfo = "flutter_miracl_sdk/0.6.0";
+const sdkApplicationInfo = "flutter_miracl_sdk/0.7.0";
 const pluginNotInitializedErrorMessage = "The MIRACL Trust Flutter plugin is not initialized!";
